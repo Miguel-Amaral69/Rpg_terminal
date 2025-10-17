8a261556dc7738eb622ed8863a763bf176a2c35f
Miguel-Amaral69 <miguel.martins.amaral@escola.pr.gov.br>
2025-10-17 11:11:20 -0300

    update

---
+++ app.py
@@ -0,0 +1,34 @@
+player = {
+     "1" : {
+         'nome':'Nilson',
+         'idade': 24,
+         'ryos':320.50,
+         'classes': 'Mago',
+         'familia': 'Loki',
+         'habilidades': {
+             'ataque': 10,
+             'defesa': 5,
+             'vida': 10,
+         },
+         'nivel': 100,
+         'xp': 0,
+         'equipamentos:':[2],
+         'dungeon': 1,
+     },
+     '2':{
+         'nome':'Rogerio Aldegar',
+         'idade': 26,
+         'ryos':500.00,
+         'classes': 'Guerreiro',
+         'familia': 'Mongólis',
+         'habilidades': {
+             'ataque': 15,
+             'defesa': 5,
+             'vida': 5,
+         },
+         'nivel': 1,
+         'xp': 0,
+         'equipamentos:':[2],
+         'dungeon': 1,
+           },
+}
